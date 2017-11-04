9faea7fe23cd0b77660497e95f917bb4e1d8f27d
Tobias Theobald <tobitheo@gmail.com>
2017-11-04 11:39:26 +0100

    Fixed formatting

--- webview.js
+++ webview.js
@@ -1,27 +1,27 @@
 // import path from 'path';
 module.exports = (Franz) => {
-  const getMessages = function getMessages() {
-    // get all message badges
-    const allBadges = document.querySelectorAll('.activity-indicator');
-    let directCount = 0, indirectCount = 0;
+	const getMessages = function getMessages() {
+		// get all message badges
+		const allBadges = document.querySelectorAll('.activity-indicator');
+		let directCount = 0, indirectCount = 0;
 
-    // get unread direct messages by tring to read the badge values
-    allBadges.forEach((item) => {
-      if (item.hasAttribute("data-count")) {
-        // Count for DMs should be in the data-count attribute
-        directCount += Math.max(1, +(item.getAttribute("data-count")));
-      } else {
-        // this will be the case for indirect messages
-        indirectCount++;
-      }
-    });
+		// get unread direct messages by tring to read the badge values
+		allBadges.forEach((item) => {
+			if (item.hasAttribute("data-count")) {
+				// Count for DMs should be in the data-count attribute
+				directCount += Math.max(1, +(item.getAttribute("data-count")));
+			} else {
+				// this will be the case for indirect messages
+				indirectCount++;
+			}
+		});
 
-    // set Franz badge
-    Franz.setBadge(directCount, indirectCount);
-  };
-  // check for new messages every second and update Franz badge
-  Franz.loop(getMessages);
+		// set Franz badge
+		Franz.setBadge(directCount, indirectCount);
+	};
+	// check for new messages every second and update Franz badge
+	Franz.loop(getMessages);
 
-  // inject franz.css stylesheet
-  // Franz.injectCSS(path.join(__dirname, 'service.css'));
+	// inject franz.css stylesheet
+	// Franz.injectCSS(path.join(__dirname, 'service.css'));
 };
